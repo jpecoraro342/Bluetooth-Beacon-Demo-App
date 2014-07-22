0a46d39ade2a430ea93f64ed505fa6f390b24030
Joseph Pecoraro <jpecoraro342@ufl.edu>
2014-07-22 17:27:11 -0400

    Initial commit

---
+++ Campus/Campus/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Campus
+//
+//  Created by Joseph Pecoraro on 7/22/14.
+//  Copyright (c) 2014 Hatchery Lab, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "GCPAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([GCPAppDelegate class]));
+    }
+}
